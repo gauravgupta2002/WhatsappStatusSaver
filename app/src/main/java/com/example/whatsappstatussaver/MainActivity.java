d772c9f9670302d9d9808d8b155feb87d099089f
gauravgupta2002 <gauravgupta200222@gmail.com>
2021-07-14 11:42:35 +0530

    Intial Whatsapp Status App Project

--- app/src/main/java/com/example/whatsappstatussaver/MainActivity.java
+++ app/src/main/java/com/example/whatsappstatussaver/MainActivity.java
@@ -2,21 +2,36 @@ package com.example.whatsappstatussaver;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import java.io.File;
+import java.util.ArrayList;
+
 import static android.os.Build.VERSION.SDK_INT;
 
 public class MainActivity extends AppCompatActivity {
     int requestCode=1;
+    Adapter adapter;
+    File[] files;
+    RecyclerView recyclerView;
+    SwipeRefreshLayout swipeRefreshLayout;
+
+    ArrayList<ModelClass> filesList = new ArrayList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerview);
+        swipeRefreshLayout = findViewById(R.id.swipe);
     }
 
     private void CheckPermission(){
